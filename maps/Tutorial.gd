1c1cdefb0de658341ad779e3126f1cb4e72eeb6f
Nicha <NichaRoj@gmail.com>
2022-04-27 14:26:23 +0700

    WIP

--- maps/Tutorial.gd
+++ maps/Tutorial.gd
@@ -1,10 +1,9 @@
 extends Control
 
 onready var global = get_node("/root/GlobalVariables")
-onready var _start_btn = $Page1/StartButton
 
 func _ready():
-	_start_btn.connect("pressed", self, "_on_start_pressed")
+	$StartButton.connect("pressed", self, "_on_start_pressed")
 	
 func _on_start_pressed():
 	global.has_seen_tutorial = true
